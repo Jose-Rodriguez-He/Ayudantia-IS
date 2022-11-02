6469f084fe958be1222882c26a510170145ec5b6
Jose-Rodriguez-He <jose.rodriguez1901@alumnos.ubiobio.cl>
2022-11-01 21:18:30 -0300

    Obtener producto

---
+++ models/product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minLenght: 1,
+        maxLenght: 100
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    quantity: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        minLenght: 1,
+        maxLenght: 250
+    },
+
+})
+
+module.exports = mongoose.model('product', productSchema);
